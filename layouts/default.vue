4ffa50e604e3bae55c50a263d968032622e704a1
Xuan Yan <xuanyan@xuanyan.ws>
2022-03-05 07:30:33 +0800

    add loading spinner

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,37 +4,42 @@
     :class="{ colorblind }"
   >
     <Navbar />
-    <main class="block p-4">
-      <slot />
-    </main>
-    <Modal v-model="showCheatSheet" :direction="lg ? 'right' : 'top'" :mask="!lg">
-      <CheatSheet />
-    </Modal>
-    <Modal v-model="showDashboard" direction="top">
-      <Dashboard />
-    </Modal>
-    <Modal v-model="showFailed" direction="top">
-      <Failed />
-    </Modal>
-    <Modal v-model="showHint" direction="top">
-      <Hint />
-    </Modal>
-    <Modal v-model="showLevelList" direction="top">
-      <LevelList />
-    </Modal>
-    <Modal v-model="showSettings" direction="top">
-      <Settings class="my-6" />
-    </Modal>
-    <Modal v-model="showShareDialog" direction="top">
-      <ShareDialog />
-    </Modal>
-    <Modal v-model="showVariants" direction="top">
-      <VariantLinks />
-    </Modal>
-    <Modal v-model="showHelp" direction="top">
-      <Welcome />
-    </Modal>
-    <Confetti />
+    <template v-if="loaded">
+      <main class="block p-4">
+        <slot />
+      </main>
+      <Modal v-model="showCheatSheet" :direction="lg ? 'right' : 'top'" :mask="!lg">
+        <CheatSheet />
+      </Modal>
+      <Modal v-model="showDashboard" direction="top">
+        <Dashboard />
+      </Modal>
+      <Modal v-model="showFailed" direction="top">
+        <Failed />
+      </Modal>
+      <Modal v-model="showHint" direction="top">
+        <Hint />
+      </Modal>
+      <Modal v-model="showLevelList" direction="top">
+        <LevelList />
+      </Modal>
+      <Modal v-model="showSettings" direction="top">
+        <Settings class="my-6" />
+      </Modal>
+      <Modal v-model="showShareDialog" direction="top">
+        <ShareDialog />
+      </Modal>
+      <Modal v-model="showVariants" direction="top">
+        <VariantLinks />
+      </Modal>
+      <Modal v-model="showHelp" direction="top">
+        <Welcome />
+      </Modal>
+      <Confetti />
+    </template>
+    <div v-else class="fixed inset-0 flex justify-center items-center pointer-events-none">
+      <Icon icon="mdi-loading" class="text-8xl animate-spin animate-loop" />
+    </div>
   </div>
 </template>
 
@@ -57,9 +62,14 @@ import {
 } from '@/lib/state';
 import { colorblind, initialized, levelState, markEnd, markStart } from '@/lib/storage';
 
-await initJieba();
-
 const lg = breakpoints.lg;
+const loaded = ref(false);
+
+onBeforeMount(async () => {
+  await initJieba();
+  await new Promise(resolve => setTimeout(resolve, 1e4));
+  loaded.value = true;
+});
 
 if (!initialized.value) {
   showHelp.value = true;
