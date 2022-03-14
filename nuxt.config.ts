c7f7b1591e60186c12ff1e4a2b0106104606735e
Xuan Yan <xuanyan@xuanyan.ws>
2022-03-14 14:00:04 +0800

    upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 import { defineNuxtConfig } from 'nuxt3';
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineNuxtConfig({
   ssr: false,
+  ...isProduction ? {
+    app: {
+      buildAssetsDir: '/dist/'
+    },
+  } : {},
   buildModules: [
     'nuxt-windicss',
   ],
+  vite: {
+    base: '/dist/'
+  }
 });
