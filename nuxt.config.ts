6b27b62e8214b42955ebc0304bab5ecec2dfec9a
Xuan Yan <xuanyan@xuanyan.ws>
2022-03-15 23:06:31 +0800

    fix linting issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,20 @@
 import { defineNuxtConfig } from 'nuxt3';
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineNuxtConfig({
   ssr: false,
-  ...isProduction ? {
-    app: {
-      buildAssetsDir: '/dist/'
-    },
-  } : {},
+  ...isProduction
+    ? {
+      app: {
+        buildAssetsDir: '/dist/',
+      },
+    }
+    : {},
   buildModules: [
     'nuxt-windicss',
   ],
   vite: {
-    base: '/dist/'
-  }
+    base: '/dist/',
+  },
 });
